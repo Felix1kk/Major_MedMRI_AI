9617195947f7b78c96c2f37533071f5e2e8ea3fd
Felix1kk <102288435+Felix1kk@users.noreply.github.com>
2025-06-11 11:08:54 +0530

    Update Register.py

--- pages/Register.py
+++ pages/Register.py
@@ -1,6 +1,20 @@
 import streamlit as st
 from Auth import register_user
 
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+.stApp {padding-top: 0;}
+footer .stButton {display: none;}  /* Hide the Streamlit logo */
+footer .stMetrics {display: none;}  /* Hide the Streamlit logo */
+</style>
+"""
+
+# Inject custom CSS
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 def register_page(cookies):
     st.title("📝 Register")
     email = st.text_input("Email")
