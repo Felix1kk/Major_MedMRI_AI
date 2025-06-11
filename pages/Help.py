c24c589bb71d273157bb15b0beee8fc605c19cfa
Felix1kk <102288435+Felix1kk@users.noreply.github.com>
2025-06-11 11:08:03 +0530

    Update Help.py

--- pages/Help.py
+++ pages/Help.py
@@ -1,5 +1,19 @@
 import streamlit as st
 
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+.stApp {padding-top: 0;}
+footer .stButton {display: none;}  /* Hide the Streamlit logo */
+footer .stMetrics {display: none;}  /* Hide the Streamlit logo */
+</style>
+"""
+
+# Inject custom CSS
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 def help_page():
     st.title("❓ Help & Instructions")
     st.markdown("""
